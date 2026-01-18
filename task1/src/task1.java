56bfab9f9d3a58553e15563ae501f53b76fa057d
eugenia-vel <eugenia_vel@bk.ru>
2026-01-19 00:06:31 +0300

    в задании 1 добавлена многопоточность

--- task1/src/task1.java
+++ task1/src/task1.java
@@ -1,21 +1,38 @@
+import java.util.concurrent.*;
+
 public class task1 {
-    private static String getPath(int n, int m) {
-        int pointer = 0;
-        StringBuilder result = new StringBuilder();
-        int[] array = new int[n];
-        for (int i = 0; i < n; i++){
-            array[i] = i + 1;
+    public static class Path implements Callable<String> {
+        public int n;
+        public int m;
+        public Path(int n, int m) {
+            this.n = n;
+            this.m = m;
         }
-        do {
-            result.append(array[pointer]);
-            pointer += m - 1;
-            if (pointer >= n) { pointer -= n;}
-        } while (pointer != 0);
-        return result.toString();
+        public String call() {
+            int pointer = 0;
+            StringBuilder result = new StringBuilder();
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++){
+                array[i] = i + 1;
+            }
+            do {
+                result.append(array[pointer]);
+                pointer += m - 1;
+                if (pointer >= n) { pointer -= n;}
+            } while (pointer != 0);
+            return result.toString();
+        }
     }
-    public static void main(String[] args) {
-        for (int i = 0; i < 4; i += 2) {
-            System.out.print(getPath(Integer.parseInt(args[i]), Integer.parseInt(args[i+1])));
-        }
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        Future<String> rez1 = executorService
+                .submit(new Path(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
+        Future<String> rez2 = executorService
+                .submit(new Path(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
+        String result1 = rez1.get();
+        String result2 = rez2.get();
+        System.out.print(result1);
+        System.out.println(result2);
+        executorService.shutdown();
     }
 }
